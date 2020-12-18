4522f9e04a19a09286ebb58de91e1daba9dd0329
Foreverhighness <Foreverhighness@github.com>
2020-12-18 16:23:45 +0000

    Add daily problem.

---
+++ Piece of cake/389. Find the Difference/main.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+struct Solution;
+impl Solution {
+    pub fn find_the_difference(s: String, t: String) -> char {
+        s.into_bytes()
+            .into_iter()
+            .chain(t.into_bytes().into_iter())
+            .fold(0, |acc, b| acc ^ b)
+            .into()
+    }
+}
+
+fn main() {}
